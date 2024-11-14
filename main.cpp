3b61bd205edf1da04618222dbf664546f4e0b941
Gianmarco-Cellamare <gianmarco.cellamare@marconibari.edu.it>
2024-11-14 18:32:55 +0000

    Vettori (02)

--- main.cpp
+++ main.cpp
@@ -2,10 +2,52 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int N;
+cout << "Inserisci studenti in una classe" << endl;
+cin >> N;
+vector <string> Nomi[N];
+vector <double> Voti[N];
+vector <int> Assenze[N];
+int i,indice;
+string nome;
+for (i=0; 0<=N-1); i++ {
+   cout << "Inserisci nome dello studente " << endl;
+   cin >> Nomi[i]
+   cout << "Inserisci voto dello studente " << endl;
+   cin >> Voti[i]
+   cout << "Inserisci assenze dello studente " << endl;
+   cin >> Assenze[i]
+
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+cout << "Inserisci nome dello studente di cui vuoi visualizzare i dati" << endl;
+cin >> nome;
+
+for (i=0; 0<=N-1); i++ {
+   if(Nomi[i]=nome){
+      indice=i;
+
+   }
+}
+if(indice >= 0){
+   cout<<"Nome dello studente:"<< Nomi[indice] <<"Voti dello studente:"<< Voti[indice] <<"Assenze dello studente:"<< Assenze[indice] << endl;
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
